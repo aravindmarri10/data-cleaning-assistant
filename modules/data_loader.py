74b42f0be2896d2f2830fc9c65bb46d2e34b7fd2
Aravind R <aravindsankar1998@gmail.com>
2025-08-06 07:36:13 +0530

    fix error

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -3,23 +3,13 @@ import numpy as np
 import streamlit as st
 
 def load_data(file):
-    try:
-        if isinstance(file, str):  # Local sample path
+    if "file_name" not in st.session_state or st.session_state.file_name != file.name:
+        try:
             df = pd.read_csv(file)
-            st.session_state.file_name = file
-        else:
-            if "file_name" not in st.session_state or st.session_state.file_name != file.name:
-                df = pd.read_csv(file)
-                st.session_state.file_name = file.name
-            else:
-                df = st.session_state.df.copy()
-
-        df = df.replace(['-', 'n/a', 'N/A', 'missing'], np.nan)
-        st.session_state.df = df
-        st.session_state.raw_data = df.copy()
-
-    except Exception as e:
-        st.error(f"❌ Failed to load file: {e}")
-        raise e
-
+            df = df.replace(['-', 'n/a', 'N/A', 'missing'], np.nan)
+            st.session_state.df = df
+            st.session_state.raw_data = df.copy()
+            st.session_state.file_name = file.name
+        except Exception as e:
+            st.error(f"❌ Failed to load file: {e}")
     return st.session_state.df, st.session_state.raw_data
